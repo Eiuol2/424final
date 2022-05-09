f7fc1a338de5afde078f7c2b24ea9571bf52065e
emmagracekim <ekimsubs@gmail.com>
2022-05-08 21:49:34 -0700

    mui and css styling for landing page and sign-in

--- portfolio-app/src/Home.js
+++ portfolio-app/src/Home.js
@@ -1,14 +1,97 @@
-import React from "react";
+import React from "react"
+import "./Home.css"
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles"
+import { Button, Grid } from "@mui/material"
+import { useHistory } from "react-router-dom"
+import WebFont from "webfontloader"
+
+const useStyles = makeStyles({
+  customButton: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    textColor: "#ffffff",
+    text: "white",
+  },
+})
 
 function Home() {
+  const classes = useStyles()
+
+  const styleObj = {
+    fontSize: 80,
+    color: "#6464DE",
+    textAlign: "center",
+    paddingTop: "100px",
+    fontWeight: "bold",
+    fontFamily: "Bungee Inline",
+  }
+
+  const history = useHistory()
+
+  const routeChangeSignUp = () => {
+    let path = "/signup"
+    history.push(path)
+  }
+
+  const routeChangeSignIn = () => {
+    let path = "/signin"
+    history.push(path)
+  }
+
   return (
     <div>
       <main>
-        <h1> Welcome to Resumix! </h1>
-        <p> This is the homepage. description goes here...</p>
+        <h1 style={styleObj}> Welcome to Resumix! </h1>
+        <p
+          style={{
+            fontFamily: "Raleway",
+            color: "white",
+            fontSize: 20,
+            textAlign: "center",
+          }}
+        >
+          {" "}
+          Here at Resumix, we provide a superior technology tool that allows you
+          to quickly and easily show off your career focused portfolio.
+          <br></br>
+          <br></br>
+          Please sign up or sign in to get started!
+        </p>
+        <Grid
+          container
+          justifyContent="center"
+          columnSpacing={3}
+          display="flex"
+          direction="row"
+          alignItems="center"
+        >
+          <Grid item>
+            <Button
+              size="large"
+              className={classes.customButton}
+              onClick={routeChangeSignUp}
+            >
+              Sign Up
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              size="large"
+              className={classes.customButton}
+              onClick={routeChangeSignIn}
+            >
+              Sign In
+            </Button>
+          </Grid>
+        </Grid>
       </main>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
--- portfolio-app/src/MyApp.js
+++ portfolio-app/src/MyApp.js
@@ -1,91 +1,104 @@
-import Resume from "./components/Resume";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import "./styling/MyApp.css";
-import { Container, Row, Col, Nav, Navbar } from "react-bootstrap";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
-import CreatePost2 from "./components/CreatePost2";
-import EditPost from "./components/EditPost";
-import PostsList from "./components/PostsList";
-import SignUp from "./components/SignUp";
-import SignIn from "./components/SignIn";
-import { useCookies } from "react-cookie";
-import Intropage from "./components/IntroPage";
-import CreateProfile from "./components/CreateProfile";
-import ProfilePage from "./components/ProfilePage";
+import Resume from "./components/Resume"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useEffect } from "react"
+import "./styling/MyApp.css"
+import { Container, Row, Col, Nav, Navbar } from "react-bootstrap"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import CreatePost2 from "./components/CreatePost2"
+import EditPost from "./components/EditPost"
+import PostsList from "./components/PostsList"
+import SignUp from "./components/SignUp"
+import SignIn from "./components/SignIn"
+import { useCookies } from "react-cookie"
+import Intropage from "./components/IntroPage"
+import CreateProfile from "./components/CreateProfile"
+import ProfilePage from "./components/ProfilePage"
+import Home from "./Home"
+import ParticlesBg from "particles-bg"
+import "./style.css"
+import WebFont from "webfontloader"
 
 function MyApp() {
-  const [cookies, setCookie, removeCookie] = useCookies(["auth_token"]);
+  const [cookies, setCookie, removeCookie] = useCookies(["auth_token"])
 
   function setToken(token) {
-    setCookie("auth_token", token, { maxAge: 1800, path: "/" });
-    console.log("This is our current token: " + token);
-    console.log("This is the cookie after being set: " + cookies.auth_token);
+    setCookie("auth_token", token, { maxAge: 1800, path: "/" })
+    console.log("This is our current token: " + token)
+    console.log("This is the cookie after being set: " + cookies.auth_token)
   }
-  console.log("This is cookies: " + cookies);
-  console.log("THis is cookies 0th idnex: " + JSON.stringify(cookies));
+  console.log("This is cookies: " + cookies)
+  console.log("THis is cookies 0th idnex: " + JSON.stringify(cookies))
 
   function logout() {
-    console.log("Here");
-    removeCookie("auth_token");
-    console.log(cookies === undefined);
-    console.log(cookies[0]);
+    console.log("Here")
+    removeCookie("auth_token")
+    console.log(cookies === undefined)
+    console.log(cookies[0])
   }
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: [
+          "Bungee Inline",
+          "Chilanka",
+          "Epilogue",
+          "Poiret One",
+          "Raleway",
+        ],
+      },
+    })
+  }, [])
 
   return (
     <div className="App">
       <Router>
-        <header className="App-header">
-          <Navbar bg="dark" variant="dark">
-            <Container>
-              <Navbar.Brand>
-                <Link to={"/"} className="nav-link">
-                  Resumix
-                </Link>
-              </Navbar.Brand>
-              <Nav className="justify-content-end">
-                {cookies.auth_token && (
-                  <Nav>
-                    <Link to={"/posts-list"} className="nav-link">
-                      Posts List
-                    </Link>
-                  </Nav>
-                )}
-                {cookies.auth_token && (
-                  <Nav>
-                    <Link to={"/resume"} className="nav-link">
-                      Resume
-                    </Link>
-                  </Nav>
-                )}
-                {!cookies.auth_token && (
-                  <Nav>
-                    <Link to={"/signup"} className="nav-link">
-                      Sign Up
-                    </Link>
-                  </Nav>
-                )}
-                {!cookies.auth_token && (
-                  <Nav>
-                    <Link to={"/signin"} className="nav-link">
-                      Sign In
-                    </Link>
-                  </Nav>
-                )}
-                {cookies.auth_token && (
-                  <Nav>
-                    <button onClick={logout}>Logout</button>
-                  </Nav>
-                )}
-              </Nav>
-            </Container>
-          </Navbar>
-        </header>
+        {/* <header className="App-header"> */}
+        <Navbar bg="dark" variant="dark" fixed="top">
+          <Container>
+            <Navbar.Brand>
+              <Link to={"/"} className="nav-link">
+                Resumix
+              </Link>
+            </Navbar.Brand>
+            <Nav className="justify-content-end">
+              {cookies.auth_token && (
+                <Nav>
+                  <Link to={"/posts-list"} className="nav-link">
+                    Posts List
+                  </Link>
+                </Nav>
+              )}
+              {cookies.auth_token && (
+                <Nav>
+                  <Link to={"/resume"} className="nav-link">
+                    Resume
+                  </Link>
+                </Nav>
+              )}
+              {!cookies.auth_token && (
+                <Nav>
+                  <Link to={"/signup"} className="nav-link">
+                    Sign Up
+                  </Link>
+                </Nav>
+              )}
+              {!cookies.auth_token && (
+                <Nav>
+                  <Link to={"/signin"} className="nav-link">
+                    Sign In
+                  </Link>
+                </Nav>
+              )}
+              {cookies.auth_token && (
+                <Nav>
+                  <button onClick={logout}>Logout</button>
+                </Nav>
+              )}
+            </Nav>
+          </Container>
+        </Navbar>
+        {/* </header> */}
         <Container>
           <Row>
             <Col md={12}>
@@ -93,7 +106,8 @@ function MyApp() {
                 <Switch>
                   {!cookies.auth_token && (
                     <Route exact path="/">
-                      <Intropage />
+                      <Home />
+                      <ParticlesBg type="circle" bg={true} />
                     </Route>
                   )}
                   {cookies.auth_token && (
@@ -153,7 +167,7 @@ function MyApp() {
         </Container>
       </Router>
     </div>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
