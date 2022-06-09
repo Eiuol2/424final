2d06c2370e26769bfa3ecf42feb263fa5b9b968d
andrewtran127 <59572748+andrewtran127@users.noreply.github.com>
2022-06-08 17:36:47 -0700

    Expanded the final statement for test cases

--- portfolio-app/cypress/integration/e2e-testing.js
+++ portfolio-app/cypress/integration/e2e-testing.js
@@ -9,8 +9,16 @@ describe('Add new post', () => {
         it('WHEN I enter title, description, content and submit the form', () => {
             cy.intercept('POST', 'http://localhost:5016/posts/create-post').as('createPost');
             cy.get('form').within(() => {
-                cy.get('input[name="title"]').type('Testing Post')
+                cy.get('input[name="title"]').type('Testing Post');
+                cy.get('input[name="describe"]').type('This is a random text to test the post');
+                cy.get('input[name=content"]').type('https://teacherdanmax.files.wordpress.com/2013/09/school-test.png');
+                cy.get('input[type="button"]').click();
             })
+            cy.wait("@createPost");
+        })
+
+        it('THEN a new post has been added to the post list', () => {
+            cy.visit('http://localhost:3000/posts-list');
         })
     })
 })
